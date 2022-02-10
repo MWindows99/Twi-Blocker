4f2805c0f7f1151e55c49128c37ab1d97e7dd45e
うぃーうぉん <80945906+MWindows99@users.noreply.github.com>
2022-02-10 11:44:38 +0900

    Bug Fix
    Changed to avoid rate limit.

--- main.py
+++ main.py
@@ -11,19 +11,26 @@ words = "裏アカ女子"
 # Create Object
 auth = tweepy.OAuthHandler(API_KEY, API_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-api = tweepy.API(auth)
+api = tweepy.API(auth,wait_on_rate_limit=True)
 
+# Start
 print("Collecting follower information...")
 follower = tweepy.Cursor(api.get_follower_ids, cursor = -1).items()
 follower_list = []
-
 for followers in follower:
     follower_list.append(followers)
+lists = len(follower_list)
+if lists == 0:
+    print("Error!")
+    return
 
+# Check
 print("Checking follower description...")
 while len(follower_list) > 0:
     user_check = follower_list.pop(0)
+    now_lists = lists - len(follower_list)
     description = str(api.get_user(user_id=user_check).description)
+    print("Checking... (" + str(now_lists) + " of " + str(lists))
     if words in description:
         #block
         api.create_block(user_id=user_check)
